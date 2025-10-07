9d45eb2c15cfcb0cacebe3776c74da8eda3ca256
MariaEmilia611 <maria945duarteferreira@outlook.com>
2025-10-07 16:07:09 -0300

    ADD interatividade

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Seletores dos elementos por ID
+
+const elements = {
+    promptTitle : document.getElementById('prompt-title'),
+    promptContent: document.getElementById('prompt-content'),
+    titleWrapper: document.getElementById('title-wrapper'),
+    contentWrapper: document.getElementById('content-wrapper'),
+};
+
+// Atualiza o estado do wrapper conforme o contéudo do elemento
+
+function updateEditableWrapperState(element, wrapper) {
+    if (!element)
+}
